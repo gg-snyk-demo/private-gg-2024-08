57c1e42f2a5ee97e6d84726ac0ff1eadfbf7389e
Bruce Banner <bruce-banner-snyk@protonmail.com>
2024-08-07 10:04:18 +0200

    Updating app.py

--- app.py
+++ app.py
@@ -1,12 +1,6 @@
 import antigravity
 
-CERT="""-----BEGIN EC PRIVATE KEY-----
-MIGkAgEBBDAENPSQpPYowuRn2IW03/SCs3kW6J/NRg2yX5GEA30NwzUcZPU72oOI
-G5AZEuSgxkagBwYFK4EEACKhZANiAAS+COKmtv45wCTsAxxW10wTj4wwC56BETio
-9LqZLRpFXjFf6wxxxzDSPNqBCn6/J4OmTjkm4v4mcTCAMPLpF9/0AEAc2TexZeWm
-kodwTPVlyQvi3zUrZfghMet/5fwo9dk=
------END EC PRIVATE KEY-----
-"""
+SEED=80d0e5f0-8ce2-4627-8435-041897489a3d
 
 def main():
     antigravity.fly()
