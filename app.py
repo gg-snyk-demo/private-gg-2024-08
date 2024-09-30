edd9f507f08f9a3e12055fc6ccb31f2461dbb5bd
Bruce Banner <bruce-banner-snyk@protonmail.com>
2024-09-30 05:03:29 +0200

    Updating app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import antigravity
 
-SEED=879329a4-7c92-4388-972e-dbfb7198a1e8
+client_id=3MVG9ZKxtgsdgQs6a.OvoIN6jXhvbMcMD.pIbZ2Cy5VjVP7cgEtVSkbzIZxntUMa.BIGCyzp_f2T0kLhLSMe8
+client_secret=9559743975704233313
 
 def main():
     antigravity.fly()
